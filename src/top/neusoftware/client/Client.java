8009dbbea98230e1505def23a3d4a7434853fbe7
aopstudio <cenzhedong2@126.com>
2018-01-16 16:18:07 +0800

    fixed an error

--- src/top/neusoftware/client/Client.java
+++ src/top/neusoftware/client/Client.java
@@ -1,21 +1,86 @@
 package top.neusoftware.client;
 
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.StringTokenizer;
-import java.io.*;
 
 public class Client {
-	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket s=new Socket("2001:67c:1560:8001::14",21);
-		Socket dataSocket;
-		BufferedReader sin=new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader reader=new BufferedReader(new InputStreamReader(s.getInputStream()));
-		PrintWriter writer=new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
-		String response;
-		String command=null;
-		writer.print("Hello");
+	Socket s;
+	Socket dataSocket;
+	BufferedReader sin;
+	BufferedReader reader;
+	PrintWriter writer;
+	String response;
+	String command;
+	public Client() throws UnknownHostException, IOException {
+		s=new Socket("127.0.0.1",21);
+		sin=new BufferedReader(new InputStreamReader(System.in));
+		reader=new BufferedReader(new InputStreamReader(s.getInputStream()));
+		writer=new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
+	}
+	
+	public void firstAccess() throws IOException {
+		writer.println("Hello");
 		writer.flush();
 		response=reader.readLine();
 		System.out.println(response);
 	}
+	public void sendCommand() throws UnknownHostException, IOException {
+		firstAccess();
+		while(!(command=sin.readLine()).equals("QUIT")){
+			if(!command.equals("PASV")) {
+				writer.println(command);
+				writer.flush();
+				response=reader.readLine();
+				System.out.println(response);
+			}
+			else {
+				writer.println(command);
+				writer.flush();
+				response=reader.readLine();
+				System.out.println(response);
+				if(response.startsWith("227")) {
+					StringTokenizer tk=new StringTokenizer(response,",");
+					for(int i=0;i<4;i++) {
+						tk.nextToken();
+					}
+					int p1=Integer.valueOf(tk.nextToken());
+					String str2=tk.nextToken();
+					int p2=Integer.valueOf(str2.substring(0,str2.length()-1));
+					int dpNumber=p1*256+p2;
+					System.out.println(dpNumber);
+					dataSocket=new Socket("127.0.0.1",dpNumber);
+				}
+				else {
+					continue;
+				}
+			}
+		}
+		writer.println(command);
+		writer.flush();
+	}
+	public void getFile() throws UnsupportedEncodingException, IOException {
+		DataInputStream dis = new DataInputStream(dataSocket.getInputStream());
+		String s = "";
+		while ((s = dis.readLine()) != null) {
+		String l = new String(s.getBytes("ISO-8859-1"), "utf-8");
+		System.out.println(l);
+		}
+	}
+	public void sendFile() {
+		FileInputStream fis = null;
+		
+	}
+	/*writer.println("USER root");
+	writer.flush();
+	response=reader.readLine();
+	System.out.println(response);*/
 }
--- src/top/neusoftware/client/Main.java
+++ src/top/neusoftware/client/Main.java
@@ -11,52 +11,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket s=new Socket("2001:67c:1560:8001::14",21);
-		Socket dataSocket;
-		BufferedReader sin=new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader reader=new BufferedReader(new InputStreamReader(s.getInputStream()));
-		PrintWriter writer=new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
-		String response;
-		String command=null;
-		writer.print("Hello");
-		writer.flush();
-		response=reader.readLine();
-		System.out.println(response);
-		
-		while(!(command=sin.readLine()).equals("QUIT")){
-			if(!command.equals("PASV")) {
-				writer.println(command);
-				writer.flush();
-				response=reader.readLine();
-				System.out.println(response);
-			}
-			else {
-				writer.println(command);
-				writer.flush();
-				response=reader.readLine();
-				System.out.println(response);
-				if(response.startsWith("227")) {
-					StringTokenizer tk=new StringTokenizer(response,",");
-					for(int i=0;i<4;i++) {
-						tk.nextToken();
-					}
-					int p1=Integer.valueOf(tk.nextToken());
-					String str2=tk.nextToken();
-					int p2=Integer.valueOf(str2.substring(0,str2.length()-1));
-					int dpNumber=p1*256+p2;
-					System.out.println(dpNumber);
-					dataSocket=new Socket("2001:67c:1560:8001::14",dpNumber);
-				}
-				else {
-					continue;
-				}
-			}
-		}
-		writer.println(command);
-		writer.flush();
-		/*writer.println("USER root");
-		writer.flush();
-		response=reader.readLine();
-		System.out.println(response);*/
+		Client cli=new Client();
+		cli.sendCommand();
 	}
 }
